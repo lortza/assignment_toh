d1c6520daea2f72e5403b49f515245e9d882d2e9
anne <richardson.ae@gmail.com>
2017-09-05 15:57:37 -0500

    Add border to title block

--- ascii_art.rb
+++ ascii_art.rb
@@ -1,23 +1,23 @@
 module AsciiArt
 
  TITLE = <<-art
-=========WELCOME=TO=THE==========
-
-XXXXX   XXX   X   X  XXXXX  XXX
-  X    X   X  X   X  X      X  X
-  X    X   X  X X X  XXXX   XXX
-  X    X   X  XX XX  X      X  X
-  X     XXX    X X   XXXXX  X   X
-
-<<<<<<<<<<<<<<< OF >>>>>>>>>>>>>>>
-
-X   X    X    XXXXX   XXX   XXXXX
-X   X   X X   X   X  X   X    X
-X X X  X   X  X   X  X   X    X
-X   X  XXXXX  X   X  X   X    X
-X   X  X   X  X   X   XXX   XXXXX
-
-==============(LITE)==============
+============WELCOME=TO=THE=============
+|                                     |
+|  XXXXX   XXX   X   X  XXXXX  XXX    |
+|    X    X   X  X   X  X      X  X   |
+|    X    X   X  X X X  XXXX   XXX    |
+|    X    X   X  XX XX  X      X  X   |
+|    X     XXX    X X   XXXXX  X   X  |
+|                                     |
+|  <<<<<<<<<<<<<< OF >>>>>>>>>>>>>>>  |
+|                                     |
+|  X   X    X    XXXXX   XXX   XXXXX  |
+|  X   X   X X   X   X  X   X    X    |
+|  X X X  X   X  X   X  X   X    X    |
+|  X   X  XXXXX  X   X  X   X    X    |
+|  X   X  X   X  X   X   XXX   XXXXX  |
+|                                     |
+================(LITE)=================
 
 art
 
